7a2683926dc6aeecf8b4e1d939096684b7bd556a
Usama Sattar <usamasattar.3347@gmail.com>
2025-12-21 23:55:21 +0100

    Created script for synthetic data generation

--- src/utils/generate_synth_data.py
+++ src/utils/generate_synth_data.py
@@ -1,9 +1,9 @@
 import blenderproc as bproc
 
-from collections import namedtuple
 import json
-import math
+from pathlib import Path
 
+import cv2
 import numpy as np
 
 
@@ -11,29 +11,52 @@ CLASS_MAP = {
     "medium": 1,
     "small": 2,
 }
+OBJ_PATH = {
+    "medium": "data/medium_box/medium_box_mesh.ply",
+    "small": "data/small_box/small_box_mesh.ply",
+}
+
+
+def sample_pose(obj: bproc.types.MeshObject):
+    n = 0
+    while n < 100:
+        u = np.random.uniform(0, image_width)
+        v = np.random.uniform(0, image_height)
+        z = np.random.choice(np.linspace(0.0, 0.01 * 110, num=111))
+
+        x = (u - cx) * z / fx
+        y = (v - cy) * z / fy
+
+        rot = np.random.choice([0, 1, 2, 3])
+        diff = np.random.uniform(-0.1, 0.1)
+        obj.set_location(
+            (np.linalg.inv(world_to_cam_transform) @ np.array([x, y, z, 1]).T)[:3]
+        )
+        obj.set_rotation_euler([0, 0, rot * np.pi / 2 + diff])
+
+        bbox = obj.get_bound_box()
+
+        bbox = np.hstack([bbox, np.ones((bbox.shape[0], 1))]) @ world_to_cam_transform.T
+        bbox = bbox[:, :3]
+
+        bbox[:, 0] = fx * (bbox[:, 0] / bbox[:, 2]) + cx
+        bbox[:, 1] = fy * (bbox[:, 1] / bbox[:, 2]) + cy
+
+        valid = True
+        for u, v, _ in bbox:
+            if not ((0 < u < image_width) and (0 < v < image_height)):
+                n += 1
+                valid = False
+                break
+
+        if not valid:
+            continue
+
+        return
+    print("No solution found")
+
 
 bproc.init()
-
-plane = bproc.object.create_primitive("PLANE")
-plane.set_location([0, 0.65, -1.1])
-plane.set_scale([2, 2, 2])
-
-# Create a simple object:
-BoxObjects = namedtuple("BoxObjects", ["medium", "small"])
-objs = BoxObjects(
-    bproc.loader.load_obj("data/medium_box/medium_box_mesh.ply")[0],
-    bproc.loader.load_obj("data/small_box/small_box_mesh.ply")[0],
-)
-
-objs.medium.set_scale(np.full((3), 1 / 1000))
-objs.medium.set_cp("category_id", CLASS_MAP["medium"])
-objs.small.set_scale(np.full((3), 1 / 1000))
-objs.small.set_cp("category_id", CLASS_MAP["small"])
-
-objs.medium.set_location([0, 0.75, 0.150])
-objs.medium.set_rotation_euler([0, 0, math.pi])
-objs.small.set_location([0.2, 0.45, 0.150])
-objs.small.set_rotation_euler([0, 0, math.pi])
 
 # Create a point light next to it
 light = bproc.types.Light()
@@ -49,39 +72,74 @@ K_matrix[0, 0] = cam_intrinsics["fx"]
 K_matrix[1, 1] = cam_intrinsics["fy"]
 K_matrix[0, 2] = cam_intrinsics["cx"]
 K_matrix[1, 2] = cam_intrinsics["cy"]
-bproc.camera.set_intrinsics_from_K_matrix(
-    K_matrix, cam_intrinsics["width"], cam_intrinsics["height"]
-)
+
+image_width = cam_intrinsics["width"]
+image_height = cam_intrinsics["height"]
+
+bproc.camera.set_intrinsics_from_K_matrix(K_matrix, image_width, image_height)
+
+fx, fy = K_matrix[0, 0], K_matrix[1, 1]
+cx, cy = K_matrix[0, 2], K_matrix[1, 2]
 
 with open("data/medium_box/cam2root.json", "r") as f:
-    cam_extrinsics = json.load(f)
+    world_to_cam_transform = json.load(f)
 
-cam_extrinsics = bproc.math.change_source_coordinate_frame_of_transformation_matrix(
-    cam_extrinsics["cam2root"], ["X", "-Y", "-Z"]
+world_to_cam_transform = (
+    bproc.math.change_source_coordinate_frame_of_transformation_matrix(
+        world_to_cam_transform["cam2root"], ["X", "-Y", "-Z"]
+    )
 )
-bproc.camera.add_camera_pose(cam_extrinsics)
-
-# Render the scene
-bproc.renderer.set_noise_threshold(0.1)
+bproc.camera.add_camera_pose(world_to_cam_transform)
 bproc.renderer.enable_depth_output(False)
-bproc.renderer.enable_segmentation_output(default_values={"category_id": 0})
-data = bproc.renderer.render()
 
-data["depth"][0][data["depth"][0] > 10.0] = 0.0
+# Create a simple object:
+total_boxes = 6
+dataset_size = 2
+np.random.seed()
+objs = []
+for i in range(dataset_size):
+    bproc.object.delete_multiple(objs)
+    objs = []
+    n_small_boxes = np.random.randint(0, total_boxes + 1)
 
-# Write the rendering into an hdf5 file
-# bproc.writer.write_hdf5("data/synthetic_data/", data)
-bproc.writer.write_coco_annotations(
-    "data/synthetic_data/",
-    instance_segmaps=data["category_id_segmaps"],
-    instance_attribute_maps=data["instance_attribute_maps"],
-    colors=data["colors"],
-    color_file_format="JPEG",
-)
-bproc.writer.write_coco_annotations(
-    "data/synthetic_data/",
-    instance_segmaps=data["category_id_segmaps"],
-    instance_attribute_maps=data["instance_attribute_maps"],
-    colors=data["depth"],
-    color_file_format="PNG",
-)
+    for j in range(1, n_small_boxes + 1):
+        obj = bproc.loader.load_obj(OBJ_PATH["small"])
+        obj[0].set_scale(np.full((3), 1 / 1000))
+        obj[0].set_cp("category_id", CLASS_MAP["small"])
+        obj[0].set_cp("instance_id", j)
+        objs.extend(obj)
+
+    for j in range(n_small_boxes + 1, total_boxes + 1):
+        obj = bproc.loader.load_obj(OBJ_PATH["medium"])
+        obj[0].set_scale(np.full((3), 1 / 1000))
+        obj[0].set_cp("category_id", CLASS_MAP["medium"])
+        obj[0].set_cp("instance_id", j)
+        objs.extend(obj)
+
+    bproc.object.sample_poses(
+        objs, sample_pose_func=sample_pose, objects_to_check_collisions=objs
+    )
+
+    # Render the scene
+    bproc.renderer.set_noise_threshold(0.1)
+    bproc.renderer.enable_segmentation_output(
+        map_by=["instance_id", "category_id"],
+        default_values={"category_id": 0, "instance_id": 0},
+    )
+    data = bproc.renderer.render()
+
+    data["depth"][0][data["depth"][0] > 10.0] = 0.0
+    data["depth"][0] = (np.round(data["depth"][0] * 1000)).astype(np.uint16)
+
+    # Write the rendering into an hdf5 file
+    path = "data/synthetic_data/"
+    bproc.writer.write_coco_annotations(
+        path,
+        instance_segmaps=data["instance_id_segmaps"],
+        instance_attribute_maps=data["instance_attribute_maps"],
+        colors=data["colors"],
+        color_file_format="JPEG",
+    )
+    bproc.writer.write_hdf5(
+        path + "/depth/", {"depth": data["depth"]}, append_to_existing_output=True
+    )
